f43cabfa7d3675b6ef3f7b46643be059ab0572c0
Sven Stucki <git@svenstucki.ch>
2017-11-30 00:02:49 +0100

    Add SafeMath.sol from zeppelin-solidity

---
+++ contracts/SafeMath.sol
@@ -0,0 +1,38 @@
+pragma solidity ^0.4.18;
+
+// From:
+// https://raw.githubusercontent.com/OpenZeppelin/zeppelin-solidity/815d9e1f457f57cfbb1b4e889f2255c9a517f661/contracts/math/SafeMath.sol
+
+
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+      return 0;
+    }
+    uint256 c = a * b;
+    assert(c / a == b);
+    return c;
+  }
+
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
+
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
