b67b4d966f38230e3fcdee991d27dc014432dfaa
Sven Stucki <git@svenstucki.ch>
2018-01-22 11:45:44 +0100

    Fix ERC20 interface definition

--- contracts/ValidToken.sol
+++ contracts/ValidToken.sol
@@ -6,13 +6,14 @@ pragma solidity ^0.4.18;
  */
 contract ERC20 {
     uint256 public totalSupply;
-    function transfer(address _to, uint _value) public returns (bool success);
-    function transferFrom(address _from, address _to, uint _value) public returns (bool success);
-    function approve(address _spender, uint _value) public returns (bool success);
-    function allowance(address _owner, address _spender) public view returns (uint remaining);
+    function balanceOf(address _owner) public view returns (uint256 balance);
+    function transfer(address _to, uint256 _value) public returns (bool success);
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
+    function approve(address _spender, uint256 _value) public returns (bool success);
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining);
 
-    event Transfer(address indexed _from, address indexed _to, uint _value);
-    event Approval(address indexed _owner, address indexed _spender, uint _value);
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 }
 
 
